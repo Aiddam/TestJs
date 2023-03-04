b6f5084a7100af02f78757c82c471cf5de21e130
Anton <aiddamaddominator@gmail.com>
2023-03-04 19:44:25 +0200

    Changed the basic logic of the application, namely the ability to add points and change the radius dynamically, added a function to calculate the radius of the circle on the two points, also added a modal window to display the information in text.

--- Logic/main.js
+++ Logic/main.js
@@ -1,8 +1,8 @@
 "use strict";
+
+//#region Canvas
 const canvas = document.querySelector("canvas");
 const context = canvas.getContext("2d");
-
-
 
 const canvasOffsets = canvas.getBoundingClientRect();
 const offsetX = canvasOffsets.left;
@@ -19,9 +19,15 @@ let is_dragging = false;
 let startX;
 let startY;
 
+canvas.onmousedown = mouseDownHandler;
+canvas.onmouseup = mouseUpHandler;
+canvas.onmouseout = mouseOutHandler;
+canvas.onmousemove = mouseMoveHandler;
+
 function clickButtonHandler(){
     shapes = [];
     context.clearRect(0, 0, canvas_width, canvas_height);
+
     let aX = document.getElementById("Ax").value;
     let bX = document.getElementById("Bx").value;
     let cX = document.getElementById("Cx").value;
@@ -33,32 +39,37 @@ function clickButtonHandler(){
     let dY = document.getElementById("Dy").value;
     
     
-    let radiusA = Math.round(Math.sqrt((aX - bX) ** 2 + ((aY - bY) ** 2)));
-    let radiusC = Math.round(Math.sqrt((cX - dX) ** 2 + ((cY - dY) ** 2)));
-    shapes.push({ x: parseInt(aX), y: parseInt(aY), radius: radiusA, color: "blue" });
-    shapes.push({ x: parseInt(cX), y: parseInt(cY), radius: radiusC, color: "yellow" });
+    let A = { x: parseInt(aX), y: parseInt(aY), radius: 2.5, color: "black" };
+    let B = { x: parseInt(bX), y: parseInt(bY), radius: 2.5, color: "black" };
+    let C = { x: parseInt(cX), y: parseInt(cY), radius: 2.5, color: "black" };
+    let D = { x: parseInt(dX), y: parseInt(dY), radius: 2.5, color: "black" };
+
+    shapes.push({ x: A.x, y: A.y, radius: getRadius(A,B), color: "blue", center: A, circleBorder: B});
+    shapes.push({ x: C.x, y: C.y, radius: getRadius(C,D), color: "yellow", center: C, circleBorder: D});
+
+    shapes.push(A);
+    shapes.push(B);
+    shapes.push(C);
+    shapes.push(D);
+
     drawShapes();
 }
-
-
-canvas.onmousedown = mouseDownHandler;
-canvas.onmouseup = mouseUpHandler;
-canvas.onmouseout = mouseOutHandler;
-canvas.onmousemove = mouseMoveHandler;
+function getRadius(firstShape, secondShape){
+    return Math.round(Math.sqrt((firstShape.x - secondShape.x) ** 2 + ((firstShape.y - secondShape.y) ** 2)));
+}
 
 function getIsMouseInShape(x, y, shape) {
-    console.log(Math.sqrt((x - shape.x) ** 2 + ((y - shape.y) ** 2)));
     return shape.radius > Math.sqrt((x - shape.x) ** 2 + ((y - shape.y) ** 2));
 }
 
 function drawShapes() {
-    
     context.clearRect(0, 0, canvas_width, canvas_height);
     for (let shape of shapes) {
-        context.beginPath();
-        context.arc(shape.x, shape.y, shape.radius, 2 * Math.PI, 0, false);
-        context.fillStyle = shape.color;
-        context.fill();
+
+            context.beginPath();
+            context.arc(shape.x, shape.y, shape.radius, 2 * Math.PI, 0, false);
+            context.fillStyle = shape.color;
+            context.fill();
     }
 }
 
@@ -91,16 +102,67 @@ function mouseOutHandler(event) {
 
 function mouseMoveHandler(event) {
     event.preventDefault();
-    if (!is_dragging)
+    if (!is_dragging){
         return;
+    }
+
     let mouseX = event.clientX - offsetX;
     let mouseY = event.clientY - offsetY;
+    
     let dx = mouseX - startX;
     let dy = mouseY - startY;
+
     let current_shape = shapes[current_index];
-    current_shape.x += dx;
-    current_shape.y += dy;
+    
+    if(current_shape.center != null && current_shape.circleBorder != null){
+        if(!isNaN(current_shape.radius)){
+            current_shape.x += dx;
+            current_shape.y += dy;
+            current_shape.center.x += dx;
+            current_shape.center.y += dy;
+            current_shape.circleBorder.x += dx;
+            current_shape.circleBorder.y += dy;
+        }
+    }
+    else{
+        for(let shape of shapes){
+            if(current_shape == shape.circleBorder){
+                current_shape = shape;
+                break;
+            }
+        }
+        if(current_shape.circleBorder != null){
+            current_shape.circleBorder.x +=dx;
+            current_shape.circleBorder.y +=dy;
+            current_shape.radius = getRadius(current_shape.center, current_shape.circleBorder);
+        }
+    }
     drawShapes();
     startX = mouseX;
     startY = mouseY;
 }
+//#endregion
+
+//#region Modal
+var modal = document.getElementById("Modal");
+var modalTitleElement = document.getElementById("modalTitle");
+var modalContentElement = document.getElementById("modalContent");
+
+function openModal() {
+    modalTitleElement.innerHTML = "The coordinates of all points";
+    modalContentElement.innerHTML = "";
+    for(let shape of shapes){
+        if(!(isNaN(shape.x) || isNaN(shape.y) || isNaN(shape.radius)))
+        {
+            var pointElement = document.createElement("p");
+            pointElement.innerHTML = "Point " + ": (" + shape.x + ", " + shape.y + ") Radius: " + shape.radius + "Color: "+shape.color;
+            modalContentElement.appendChild(pointElement);
+        }
+    }
+    modal.style.display = "block";
+  }
+  
+  function closeModal() {
+    modal.style.display = "none";
+  }
+  //#endregion
